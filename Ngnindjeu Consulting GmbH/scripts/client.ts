5a150f56385665f77fcadfe975dbaf78f0129436
Audrane Carelle Ngnindjeu <audrane.ngnindjeu@mni.thm.de>
2020-04-08 19:40:48 +0200

    Erste Version des Codes

--- Ngnindjeu Consulting GmbH/scripts/client.ts
+++ Ngnindjeu Consulting GmbH/scripts/client.ts
@@ -5,7 +5,40 @@ $(function () {
     });
 });
 
+$(function change (){
+  $('#Angestel').click(function () {
+     $(this).css("backgroundColor","green");
+  });
 
+    $('#tickets').click(function () {
+        $(this).css("backgroundColor","red");
+    });
+
+    $('.tablinks').click(function () {
+        $(this).css("backgroundColor","yellow");
+    });
+});
+
+/*
+$(function change() {
+    $('#tab').on("click",function() {
+        $('#Angestel').css('backgroundColor',"green");
+    });
+    $('#tab').on("click",function() {
+        $('#tickets').css('backgroundColor',"red");
+    });
+    $('#tab').on("click",function() {
+        $('.tablinks').css('backgroundColor',"yellow");
+    });
+});
+
+
+
+function change(){
+    document.getElementById('defaultOpen').style.backgroundColor = "white";
+   // document.getElementById('defaultOpen2').style.backgroundColor = "white";
+}
+*/
 
 // @ts-ignore
 jQuery($(".chosen-select").chosen({
